5c37fcc6e5abeef425a25259fc9aa8c555b33924
uladaharanina <uladaharanina@gmail.com>
2025-03-08 20:04:06 -0500

    update uri in second microservice

--- ReportService/Controller/ReportController.cs
+++ ReportService/Controller/ReportController.cs
@@ -2,10 +2,31 @@ using Microsoft.AspNetCore.Mvc;
 
 public class ReportController : ControllerBase
 {
+    private IReportService _service;
+    public ReportController(IReportService service)
+    {
+        _service = service;
+    }
 
     [HttpGet]
-    [Route("api/GenerateReport")]
-    public IActionResult GetReport()
+    [Route("api/GenerateWeeklyReport")]
+    public async Task<IActionResult> GetWeeklyReport()
+    {
+        //Request data
+        try
+        {
+            WeeklyReportDTO report = await _service.GenerateWeeklyReport();
+            return Ok(report);
+        }
+        catch (Exception e)
+        {
+            return StatusCode(500, e.Message);
+        }
+    }
+
+    [HttpGet]
+    [Route("api/GenerateMontlyReport")]
+    public IActionResult GetMonthlyReport()
     {
         return StatusCode(200);
     }
